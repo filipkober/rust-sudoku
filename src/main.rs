d68ffc6c9422fb08557f946459da2b04970e5924
Filip Kober <koberfilip2@gmail.com>
2024-06-15 20:30:01 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate image as im;
 use ab_glyph::{FontArc, PxScale};
 use im::{ImageBuffer, Rgba};
 fn main() {
+
+    // window, texture and board setup
+
     let opengl = OpenGL::V3_2;
 
     let (width, height) = (720, 720);
@@ -34,8 +37,12 @@ fn main() {
     let mut texture: G2dTexture =
         Texture::from_image(&mut texture_context, &canvas, &TextureSettings::new()).unwrap();
 
+
+    // running the backtracking algorithm with initial parameters
+    // TODO: move window related arguments into a struct
     solve(&mut board, 0, 0, &mut window, &mut texture, &mut texture_context, &mut canvas, cell_size, width);
 
+    // keep rendering the window until escape is pressed or program is ended
     while let Some(e) = window.next() {
         if e.render_args().is_some() {
             texture.update(&mut texture_context, &canvas).unwrap();
@@ -53,8 +60,11 @@ fn draw_grid(cell_size: u32, grid_size: u32, canvas: &mut ImageBuffer<Rgba<u8>, 
     for y in 0u32..grid_size {
         for x in 0u32..grid_size {
             let is_border = x % cell_size == 0 || y % cell_size == 0;
+
             let block_x = (x / (cell_size * 3)) % 2;
             let block_y = (y / (cell_size * 3)) % 2;
+
+            // every 3 blocks are green and then white and so on
             let is_green_block = (block_x + block_y) % 2 != 0;
 
             if is_border {
@@ -73,6 +83,8 @@ fn draw_in_grid(
     cell_size: u32,
     canvas: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
 ) {
+
+    // font setup
     let font_data = include_bytes!("../assets/FiraSans-Regular.ttf") as &[u8];
     let font = FontArc::try_from_slice(&font_data).unwrap();
     let scale = PxScale::from(cell_size as f32 / 2.0);
@@ -82,6 +94,7 @@ fn draw_in_grid(
     let cell_top_left_y = cell_y * cell_size;
 
     // Calculate the position to draw the text so it is centered within the cell
+    // theoretically there should be / 2 in both places but it didn't center it vertically
     let x = cell_top_left_x + (cell_size - text_size.0 as u32) / 3;
     let y = cell_top_left_y + (cell_size - text_size.1 as u32) / 2;
 
@@ -97,15 +110,19 @@ fn draw_in_grid(
 }
 
 fn is_valid(board: &mut [[u8; 9]; 9], row: usize, col: usize, num: u8) -> bool {
+
+    // if value is present in the row or column return false
     for i in 0..9 {
         if board[row][i] == num || board[i][col] == num {
             return false;
         };
     }
 
+    // calculate the starting index of the subgrid that the checked number is in
     let start_row = row - row % 3;
     let start_col = col - col % 3;
 
+    // check for number in subgrid
     for i in 0..3 {
         for j in 0..3 {
             if board[i + start_row][j + start_col] == num {
@@ -119,6 +136,7 @@ fn is_valid(board: &mut [[u8; 9]; 9], row: usize, col: usize, num: u8) -> bool {
 
 fn solve(board: &mut [[u8; 9]; 9], mut row: usize, mut col: usize, window: &mut PistonWindow, texture: &mut G2dTexture, texture_context: &mut G2dTextureContext, canvas: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, cell_size: u32, width: u32) -> bool {
 
+    // render board every time the function is called (there probably is a more efficient solution)
     if let Some(e) = window.next() {
         if e.render_args().is_some() {
             texture.update(texture_context, &canvas).unwrap();
@@ -150,15 +168,18 @@ fn solve(board: &mut [[u8; 9]; 9], mut row: usize, mut col: usize, window: &mut 
         }
     }
 
-
+    // if the algorithm has reached the bottom right of the grid, it means that everything is solved
     if row == 8 && col == 9 {
         return true;
     }
+
+    // move to next row if column is out of bounds
     if col == 9 {
         row += 1;
         col = 0;
     }
 
+    // if value is not set call the function recursively with every valid number
     if board[row][col] == 0 {
         for i in 1..10 {
             if is_valid(board, row, col, i) {
@@ -167,10 +188,12 @@ fn solve(board: &mut [[u8; 9]; 9], mut row: usize, mut col: usize, window: &mut 
                     return true;
                 }
             }
+            // code gets to this point if no valid solutions exist, so it resets the spot
             board[row][col] = 0
         }
         return false;
     } else {
+        // if the value is set, move to the next value
         return solve(board, row, col + 1, window, texture, texture_context, canvas, cell_size, width)
     }
 }
